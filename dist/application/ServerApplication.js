60feb60159ecb3549be6669ca5054445b7b1a612
Shubbu03 <thatcoderguyshubham@gmail.com>
2023-10-11 14:43:31 +0530

    basic edits

--- dist/application/ServerApplication.js
+++ dist/application/ServerApplication.js
@@ -10,6 +10,7 @@ const common_1 = require("@nestjs/common");
 const core_1 = require("@nestjs/core");
 const cors_1 = __importDefault(require("cors"));
 const swagger_1 = require("@nestjs/swagger");
+const express_1 = __importDefault(require("express"));
 class ServerApplication {
     constructor() {
         this.host = ApiServerConfig_1.ApiServerConfig.HOST;
@@ -18,24 +19,29 @@ class ServerApplication {
     async run() {
         const app = await core_1.NestFactory.create(_RootModule_1.RootModule);
         app.use((0, cors_1.default)({
-            origin: "*"
+            origin: "*",
         }));
+        app.use("/uploadedFiles", express_1.default.static('dist/uploadedFiles/'));
         this.buildAPIDocumentation(app);
         this.log();
         await app.listen(this.port, this.host);
     }
     buildAPIDocumentation(app) {
-        const title = 'Educatedd';
-        const description = 'Educatedd API documentation';
-        const version = '1.0.0';
+        const title = "Educatedd";
+        const description = "Educatedd API documentation";
+        const version = "1.0.0";
         const options = new swagger_1.DocumentBuilder()
             .setTitle(title)
             .setDescription(description)
             .setVersion(version)
-            .addBearerAuth({ type: 'apiKey', in: 'header', name: ApiServerConfig_1.ApiServerConfig.ACCESS_TOKEN_HEADER })
+            .addBearerAuth({
+            type: "apiKey",
+            in: "header",
+            name: ApiServerConfig_1.ApiServerConfig.ACCESS_TOKEN_HEADER,
+        })
             .build();
         const document = swagger_1.SwaggerModule.createDocument(app, options);
-        swagger_1.SwaggerModule.setup('documentation', app, document);
+        swagger_1.SwaggerModule.setup("documentation", app, document);
     }
     log() {
         common_1.Logger.log(`Server started on host: ${this.host}; port: ${this.port};`, ServerApplication.name);
--- src/application/ServerApplication.ts
+++ src/application/ServerApplication.ts
@@ -1,56 +1,73 @@
-import { RootModule } from '@application/di/.RootModule';
-import { ApiServerConfig } from '@infrastructure/config/ApiServerConfig';
-import { Logger } from '@nestjs/common';
-import { NestFactory } from '@nestjs/core';
-import { NestExpressApplication } from '@nestjs/platform-express';
+import { RootModule } from "@application/di/.RootModule";
+import { ApiServerConfig } from "@infrastructure/config/ApiServerConfig";
+import { Logger } from "@nestjs/common";
+import { NestFactory } from "@nestjs/core";
+import { NestExpressApplication } from "@nestjs/platform-express";
 // import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
-import cors from "cors"
-import { DocumentBuilder, OpenAPIObject, SwaggerModule } from '@nestjs/swagger';
+import cors from "cors";
+import { DocumentBuilder, OpenAPIObject, SwaggerModule } from "@nestjs/swagger";
+import express from "express";
 
 export class ServerApplication {
-  
   private readonly host: string = ApiServerConfig.HOST;
-  
+
   private readonly port: number = ApiServerConfig.PORT;
 
-
-  
   public async run(): Promise<void> {
-    const app: NestExpressApplication = await NestFactory.create<NestExpressApplication>(RootModule);
+    const app: NestExpressApplication =
+      await NestFactory.create<NestExpressApplication>(RootModule);
 
-    app.use(cors({
-      origin:"*"
-    }));
-    
+    app.use(
+      cors({
+        origin: "*",
+      })
+    );
+
+    // app.use("/uploadedFiles", express.static("dist"));
+
+    // const path = require("path");
+    // const x = path.join("dist", "uploadedFiles");
+    // const newPath = express.static(x);
+    app.use("/files", express.static('dist/'));
+
+    // console.log("new path::", newPath.name);
+    // console.log("PATH::::",app.use("/uploadedFiles", express.static(path.join("dist","uploadedFiles"))));
+
     this.buildAPIDocumentation(app);
     this.log();
-  
+
     await app.listen(this.port, this.host);
   }
-  
+
   private buildAPIDocumentation(app: NestExpressApplication): void {
-    const title: string = 'Educatedd';
-    const description: string = 'Educatedd API documentation';
-    const version: string = '1.0.0';
-    
-    const options: Omit<OpenAPIObject, 'paths'> = new DocumentBuilder()
+    const title: string = "Educatedd";
+    const description: string = "Educatedd API documentation";
+    const version: string = "1.0.0";
+
+    const options: Omit<OpenAPIObject, "paths"> = new DocumentBuilder()
       .setTitle(title)
       .setDescription(description)
       .setVersion(version)
-      .addBearerAuth({ type: 'apiKey', in: 'header', name: ApiServerConfig.ACCESS_TOKEN_HEADER })
+      .addBearerAuth({
+        type: "apiKey",
+        in: "header",
+        name: ApiServerConfig.ACCESS_TOKEN_HEADER,
+      })
       .build();
-    
+
     const document: OpenAPIObject = SwaggerModule.createDocument(app, options);
-    
-    SwaggerModule.setup('documentation', app, document);
+
+    SwaggerModule.setup("documentation", app, document);
   }
-  
+
   private log(): void {
-    Logger.log(`Server started on host: ${this.host}; port: ${this.port};`, ServerApplication.name);
+    Logger.log(
+      `Server started on host: ${this.host}; port: ${this.port};`,
+      ServerApplication.name
+    );
   }
-  
+
   public static new(): ServerApplication {
     return new ServerApplication();
   }
-  
 }
